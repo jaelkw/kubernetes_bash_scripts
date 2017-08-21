f3e14a499f44f191534be4154765450d2ffd6df1
Jae Lee <jaelkw@gmail.com>
2017-08-21 11:56:22 +0800

    Added Docker Installation & Uninstallation scipts

--- docker_install.sh
+++ docker_install.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+# get root permissions before doing the downloads, so that don't have to be interrupted anymore
+sudo su
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88 # Verify that the key fingerprint is 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88.
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo docker run hello-world # Verify that this runs
