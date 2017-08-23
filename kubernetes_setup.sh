d7537277e49f4d1fd2106ea23b72ea0e180cde74
Jae Lee <jaelkw@gmail.com>
2017-08-23 15:43:07 +0800

    Commented out installing docker

--- kubernetes_setup.sh
+++ kubernetes_setup.sh
@@ -6,6 +6,6 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 apt-get update
 
 # Uncomment if don't yet have docker
-apt-get install -y docker.io
+# apt-get install -y docker.io
 
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
